e46a55cbeb2ea45de105945d68058c82b20f0227
Zachary Way <zway@paytel.com>
2019-05-05 19:08:18 -0400

    added env variables

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -19,6 +19,11 @@ namespace ToDoApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(
+                    new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json")
+                        .AddEnvironmentVariables()
+                        .Build())
                 .UseStartup<Startup>();
     }
 }
